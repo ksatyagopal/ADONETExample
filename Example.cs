f12bd84f0d6309f575ea6abbfdbdf50ab673171e
Satyagopal <dev_satya@outlook.com>
2022-09-20 08:04:21 +0530

    Add project files.

---
+++ Example.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ADONETExample
+{
+    class Example
+    {
+        public static SqlConnection conn = null;
+        public static SqlCommand cmd;
+        public void SelectData()
+        {
+            try
+            {
+                if(conn == null)
+                {
+                    throw new Exception("Connected Not Initiated");
+                }
+                cmd = new SqlCommand("Select * from Employee", conn);
+                cmd.Connection = conn;
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                        Console.Write(reader[i] + "\t\t");
+                    Console.WriteLine("\n");
+                }
+                reader.Close();
+            }
+            
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception Occurred in SelectData() Method \n" + e);
+            }
+        }
+
+        public int InsertRecord(int empid, string name, string city, int deptid, float sal, int reportsto)
+        {
+            int count = 0;
+            try
+            {
+                cmd = new SqlCommand("Employee_Add", conn);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@eid", empid);
+                cmd.Parameters.AddWithValue("@ename", name);
+                cmd.Parameters.AddWithValue("@doj", DateTime.Now);
+                cmd.Parameters.AddWithValue("@city", city);
+                cmd.Parameters.AddWithValue("@did", deptid);
+                cmd.Parameters.AddWithValue("@sal", sal);
+                cmd.Parameters.AddWithValue("@repsto", reportsto);
+                count = cmd.ExecuteNonQuery();
+                
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Unable to Insert a Record\n" + e);
+            }
+            return count;
+        }
+
+        static SqlConnection InitiateDB()
+        {
+            try
+            {
+                conn = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=LearningDemodb; Integrated Security=true");
+                conn.Open();
+                Console.WriteLine("DB Initiated");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Unable to Initiate Database");
+            }
+            return conn;
+        }
+        public SqlConnection Initiate()
+        {
+            return InitiateDB();
+        }
+        public void Close()
+        {
+            conn.Close();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ADONETExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Example obj = new Example();
+            obj.Initiate();
+            obj.SelectData();
+            Console.WriteLine("Enter Employee ID:");
+            int empid = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Employee Name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Employee City:");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter Employee Salary:");
+            float salary = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Employee Department ID:");
+            int deptid = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Employee Reporting Manager ID:");
+            int reportsto = Convert.ToInt32(Console.ReadLine());
+            int recordseffected = obj.InsertRecord(empid, name, city, deptid, salary, reportsto);
+            Console.WriteLine($"{recordseffected} Row(s) Effected");
+            obj.SelectData();
+        }
+    }
+}
